8d5b37e1467655c94b90b6c02f4e0f1e926c9b98
Teng Tokoro <tokoro10g@tokor.org>
2025-02-23 14:50:08 -0800

    Lint

--- src/db.rs
+++ src/db.rs
@@ -5,5 +5,5 @@ pub async fn get_db() -> Result<DatabaseConnection, DbErr> {
     if env_result.is_err() {
         Err(DbErr::Custom("No .env file found".to_string()))?;
     }
-    Ok(sea_orm::Database::connect(std::env::var("DATABASE_URL").unwrap_or_default()).await?)
+    sea_orm::Database::connect(std::env::var("DATABASE_URL").unwrap_or_default()).await
 }
